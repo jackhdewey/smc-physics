0b2d849b0cad05eb44c7954347c343ba1c62dbee
jackhdewey <jackhdewey@gmail.com>
2023-12-18 11:28:45 -0500

    Moved plots to dedicated file

--- elasticity.jl
+++ elasticity.jl
@@ -3,9 +3,18 @@
 # Implemented as a deterministic simulation over uncertain properties
 # Two ways to provide (fix) observed values - parameters and constraints
 
+# GOAL: Perform sequential inference of elasticity using 3 seconds of observations, followed by 3 seconds of prediction
+# GOAL: Test what simulation settings (i.e. resource-rational approximations) best match human performance:
+#        * using a rigid body
+#        * using a sphere
+#        * adding noise to orientation
+# TODO: Print / write the traces to a .csv file, check whether it recovers elasticity
+# TODO: Add ceiling and fourth wall; consider different restitution values
+# DONE: Try out elasticity values 0.2 - 1.0
+# DONE: Try a sphere
+
 using Accessors
 using Distributions
-using Plots
 using Gen
 using PyCall
 using PhySMC
@@ -13,67 +22,12 @@ using PhyBullet
 bullet = pyimport("pybullet")
 pybullet_data = pyimport("pybullet_data")
 include("truncatednorm.jl")
-
-# GOAL: Perform sequential inference of elasticity using 3 seconds of observations, followed by 3 seconds of prediction
-# GOAL: Test what simulation settings (i.e. resource-rational approximations) best match human performance:
-#        * using a rigid body
-#        * using a sphere
-#        * adding noise to orientation
-# TODO: Print / write the traces to a .csv file, check whether it recovers elasticity
-# TODO: Add ceiling and fourth wall, consider different restitution values
-# DONE: Try out elasticity values 0.2 - 1.0
-# DONE: Try a sphere
-
-
-# Plots
-
-@userplot SimPlot
-@recipe function f(cp::SimPlot)
-    z, t = cp.args
-    cs = size(z, 1)
-    k = 10
-    inds = (max(1, t-k):t)
-    n = length(inds)
-    linewidth -->range(0,10, length=n)
-    seriesalpha --> range(0,1,length=n)
-    xguide --> "time"
-    xlims --> (1, 60)
-    yguide --> "height of cube (z)"
-    ylims --> (0, 4.0)
-    label --> false
-    inds, z[inds, :]
-end
-
-function get_zs(trace::Gen.Trace)
-    t, _... = get_args(trace)
-    states = get_retval(trace)
-    zs = Vector{Float64}(undef, t)
-    for i = 1:t
-        zs[i] = states[i].kinematics[1].position[3]
-    end
-    return zs
-end
-
-function animate_trace(traces::Gen.Trace; label = "trace")
-    t = first(get_args(trace))
-    zs = reshape(get_zs(trace), (t, 1))
-    @animate for i=2:t
-        simplot(zs, i, label = label)
-    end
-end
-
-function animate_traces(traces::Vector{<:Gen.Trace})
-    zzs = reduce(hcat, map(get_zs, traces))
-    t = size(zzs, 1)
-    @animate for i=2:t
-        simplot(zzs, i)
-    end
-end
+include("plots.jl")
 
 
 # Generative Model
 
-# Stipulates initial scene configuration
+# Sets initial scene configuration
 function init_scene(mass::Float64=1.0, restitution::Float64=0.9)
 
     bullet.setGravity(0, 0, -10)
@@ -114,13 +68,12 @@ function init_scene(mass::Float64=1.0, restitution::Float64=0.9)
     return sphere
 end
 
-
 # Updates latent variables
 function update_latents(latents::RigidBodyLatents, mass::Float64, res::Float64)
     RigidBodyLatents(setproperties(latents.data, mass=mass, restitution=res))
 end
 
-# Initialize estimates of mass and restitution from their priors
+# Samples initial estimates of mass and restitution
 @gen function sample_from_prior(latents::RigidBodyLatents)
     mass = {:mass} ~ gamma(1.2, 10.)
     res = {:restitution} ~ uniform(0, 1)
@@ -128,21 +81,21 @@ end
     return new_latents
 end
 
-# Extract rigid body position and add noise
+# Extracts ground truth position and adds measurement noise
 @gen function generate_observation(k::RigidBodyState)
     pos = k.position
     obs = {:position} ~ broadcasted_normal(pos, 0.1)
     return obs
 end
 
-# Deterministically generate the next state, then sample an observation given that state
+# Deterministically generates the next state, then samples an observation
 @gen function kernel(t::Int, current_state::BulletState, sim::BulletSim)
     next_state::BulletState = PhySMC.step(sim, current_state)
     {:observation} ~ Gen.Map(generate_observation)(next_state.kinematics)
     return next_state
 end
 
-# Sample latents and run complete forward simulation
+# Samples latents, then runs complete forward simulation
 @gen function simulation(T::Int, sim::BulletSim, init_state::BulletState)
     latents = {:prior} ~ Gen.Map(sample_from_prior)(init_state.latents)
     init_state = Accessors.setproperties(init_state; latents=latents)
@@ -153,7 +106,7 @@ end
 
 # Inference Procedure
 
-# Rejuvenates latent estimates
+# Rejuvenate latent estimates
 @gen function proposal(trace::Gen.Trace)
 
     # Read current mass and restitution estimates from trace
@@ -192,10 +145,10 @@ function infer(gm_args::Tuple, obs::Vector{Gen.ChoiceMap}, num_particles::Int=20
     return state.traces
 end
 
-# Tests whether different elasticity settings produce plausible trajectories
+# Tests whether a range of elasticity settings produce plausible trajectories
 function test_elasticity()
     args = (60, sim, init_state)
-    gt_constraints = choicemap((:prior => 1 => :restitution, 0.8), (:prior => 1 => :mass, 1.0))
+    gt_constraints = choicemap((9:prior => 1 => :restitution, 0.8), (:prior => 1 => :mass, 1.0))
     trace = first(generate(simulation, args, gt_constraints))
 
     traces = [trace]
@@ -225,8 +178,10 @@ function main()
     bullet.resetDebugVisualizerCamera(5, 0, -5, [0, 0, 2])
     # bullet.resetSimulation(bullet.RESET_USE_DEFORMABLE_WORLD)
 
-    # Initialize simulation context and scene
+    # Initialize simulation context 
     sim = BulletSim(; client=client)
+
+    # Initialize scene
     rb_cube = RigidBody(init_scene())
     init_state = BulletState(sim, [rb_cube])
 
