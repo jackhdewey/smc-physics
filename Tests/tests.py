e976dafe52a73cfda01a4695269b97491a1088da
jackhdewey <jackhdewey@gmail.com>
2024-01-27 10:53:39 -0500

    Added new TODOs

--- Tests/tests.py
+++ Tests/tests.py
@@ -2,6 +2,10 @@ import pybullet as p
 import pybullet_data
 import simulation
 
+# TODO: Drop sphere from 1m, vary elasticity from 0.0-1.0 with 0.1 increments, save to .csv 
+# TODO: Set elasticity of floor to 0.3
+# TODO: Rotate a cube to ~10 angles, then do the same test
+
 # TODO: Find a way to run multiple simulations, save, display as 'video' later
 def main():
     p.connect(p.GUI)
