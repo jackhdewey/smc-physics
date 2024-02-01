d4b3af1b91b8b577ebdd5edc34773919523a3f9d
jackhdewey <jackhdewey@gmail.com>
2024-02-01 09:54:55 -0500

    Implemented elasticity test, results written to test.csv

--- Tests/simulation.py
+++ Tests/simulation.py
@@ -1,7 +1,113 @@
 import pybullet as p
 import time
 import random
-import json
+import csv
+
+# TODO: Drop sphere from 1m, vary elasticity from 0.0-1.0 with 0.1 increments, save to .csv 
+# TODO: Set elasticity of floor to 0.3
+# TODO: Rotate a cube to ~10 angles, then do the same test
+
+def init_scene():
+
+    p.setGravity(0, 0, -10)
+    # cube_location, platform_location, platform_orientation = sample_init_state()
+
+    # Create and position ground plane
+    planeID = p.createCollisionShape(p.GEOM_PLANE)
+    plane = p.createMultiBody(baseCollisionShapeIndex=planeID, basePosition=[0, 0, 0])
+    p.changeDynamics(plane, -1, restitution=0.3)
+
+    # Create and position walls
+    plane2ID = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.01, 1, 1])
+    quaternion = p.getQuaternionFromEuler([0, 0, 0])
+    plane2 = p.createMultiBody(baseCollisionShapeIndex=plane2ID, basePosition=[1, 0, 1], baseOrientation=quaternion)
+    p.changeDynamics(plane2, -1, restitution=0.3)
+
+    plane3ID = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.01, 1, 1])
+    plane3 = p.createMultiBody(baseCollisionShapeIndex=plane3ID, basePosition=[-1, 0, 1], baseOrientation=quaternion)
+    p.changeDynamics(plane3, -1, restitution=0.3)
+
+    plane4ID = p.createCollisionShape(p.GEOM_BOX, halfExtents=[1, 0.01, 1])
+    plane4 = p.createMultiBody(baseCollisionShapeIndex=plane4ID, basePosition=[0, 1, 1], baseOrientation=quaternion)
+    p.changeDynamics(plane4, -1, restitution=0.3)
+
+    # Create base cube shape
+    cubeShape = p.createCollisionShape(p.GEOM_BOX, halfExtents=[.1, .1, .1])
+    cube_locations = [[-0.6, 0, 3], [-0.2, 0, 3], [0.2, 0, 3], [0.6, 0, 3]]
+    startOrientationCube = p.getQuaternionFromEuler([0, 0, 0])
+    cubeBodies = []
+    
+    # Position and orient first cube
+    startPosCube = [0, 0, 1]
+    cube = p.createMultiBody(baseCollisionShapeIndex=cubeShape, basePosition=startPosCube,
+                             baseOrientation=startOrientationCube)
+    p.changeDynamics(cube, -1, mass=1.0, restitution=0.9)
+    p.resetBaseVelocity(cube, [-.9, 0, 0])
+    cubeBodies.append(cube)
+
+    '''
+    # Position and orient second cube
+    startPosCube = cube_locations[1]
+    cube1 = p.createMultiBody(baseCollisionShapeIndex=cubeShape, basePosition=startPosCube,
+                              baseOrientation=startOrientationCube)
+    p.changeDynamics(cube1, -1, mass=1.0, restitution=0.7)
+    cubeBodies.append(cube1)
+    
+    # Position and orient third cube
+    startPosCube = cube_locations[2]
+    cube2 = p.createMultiBody(baseCollisionShapeIndex=cubeShape, basePosition=startPosCube,
+                              baseOrientation=startOrientationCube)
+    p.changeDynamics(cube2, -1, mass=1.0, restitution=0.4)
+    cubeBodies.append(cube2)
+
+    # Position and orient fourth cube
+    startPosCube = cube_locations[3]
+    cube3 = p.createMultiBody(baseCollisionShapeIndex=cubeShape, basePosition=startPosCube,
+                              baseOrientation=startOrientationCube)
+    p.changeDynamics(cube3, -1, mass=1.0, restitution=0.1)
+    cubeBodies.append(cube3)
+    
+    # Create and position soft body cubes
+    cube1 = p.loadSoftBody("soft_cube__sf.obj", simFileName="soft_cube.vtk", basePosition=cube_locations[0],
+                            mass=4, useNeoHookean=1, NeoHookeanMu=60, NeoHookeanLambda=600, NeoHookeanDamping=0.001,
+                            collisionMargin=0.006, useSelfCollision=1, frictionCoeff=0.5, repulsionStiffness=800)
+    cubeBodies.append(cube1)
+
+    cube2 = p.loadSoftBody("soft_cube__sf.obj", simFileName="soft_cube.vtk", basePosition=cube_locations[1],
+                            mass=4, useNeoHookean=1, NeoHookeanMu=280, NeoHookeanLambda=600, NeoHookeanDamping=0.001,
+                            collisionMargin=0.006, useSelfCollision=1, frictionCoeff=0.5, repulsionStiffness=700)
+    cubeBodies.append(cube2)
+    '''
+
+    return cubeBodies, cube_locations
+
+def test_elasticity(cube):
+
+    fields = ['Elasticity', 'X', 'Y', 'Z']
+
+    file = "test.csv"
+    csvfile = open(file, 'w')
+    writer = csv.writer(csvfile)
+    writer.writerow(fields)
+
+    for i in range(0, 10):
+
+        elasticity = 0.1 * i
+
+        initial_position = [0.0, 0.0, 1.0]
+        initial_position.insert(0, elasticity)
+        writer.writerow(initial_position)
+
+        for t in range(0, 200):
+            p.stepSimulation()
+            position = p.getBasePositionAndOrientation(cube)[0]
+            position = list(position)
+            position.insert(0, elasticity)
+            writer.writerow(position)        
+
+        p.resetBasePositionAndOrientation(cube, [0, 0, 1], p.getQuaternionFromEuler([0, 0, 0]))
+
+    return
 
 
 def sample_init_state():
@@ -21,97 +127,6 @@ def sample_init_state():
     platform_orientation = [0, theta_y, 0]
 
     return cube_location, platform_location, platform_orientation
-
-
-def init_scene():
-
-    p.setGravity(0, 0, -10)
-    # cube_location, platform_location, platform_orientation = sample_init_state()
-
-    # Create and position ground plane
-    planeID = p.createCollisionShape(p.GEOM_PLANE)
-    plane = p.createMultiBody(baseCollisionShapeIndex=planeID, basePosition=[0, 0, 0])
-    p.changeDynamics(plane, -1, mass=0.0, restitution=0.9)
-
-    # Create and position walls
-    plane2ID = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.01, 1, 1])
-    quaternion = p.getQuaternionFromEuler([0, 0, 0])
-    plane2 = p.createMultiBody(baseCollisionShapeIndex=plane2ID, basePosition=[1, 0, 1], baseOrientation=quaternion)
-    p.changeDynamics(plane2, -1, mass=0.0, restitution=0.9)
-
-    plane3ID = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.01, 1, 1])
-    plane3 = p.createMultiBody(baseCollisionShapeIndex=plane3ID, basePosition=[-1, 0, 1], baseOrientation=quaternion)
-    p.changeDynamics(plane3, -1, mass=0.0, restitution=0.9)
-
-    plane4ID = p.createCollisionShape(p.GEOM_BOX, halfExtents=[1, 0.01, 1])
-    plane4 = p.createMultiBody(baseCollisionShapeIndex=plane4ID, basePosition=[0, 1, 1], baseOrientation=quaternion)
-    p.changeDynamics(plane4, -1, mass=0.0, restitution=0.9)
-
-    # Create base cube shape
-    cubeShape = p.createCollisionShape(p.GEOM_BOX, halfExtents=[.1, .1, .1])
-    cube_locations = [[-0.6, 0, 3], [-0.2, 0, 3], [0.2, 0, 3], [0.6, 0, 3]]
-    startOrientationCube = p.getQuaternionFromEuler([0, 0, 0])
-    cubeBodies = []
-    
-    # Position and orient first cube
-    startPosCube = cube_locations[0]
-    cube = p.createMultiBody(baseCollisionShapeIndex=cubeShape, basePosition=startPosCube,
-                             baseOrientation=startOrientationCube)
-    p.changeDynamics(cube, -1, mass=1.0, restitution=0.9)
-    p.resetBaseVelocity(cube, [-.9, 0, 0])
-    cubeBodies.append(cube)
-
-    # Position and orient second cube
-    startPosCube = cube_locations[1]
-    cube1 = p.createMultiBody(baseCollisionShapeIndex=cubeShape, basePosition=startPosCube,
-                              baseOrientation=startOrientationCube)
-    p.changeDynamics(cube1, -1, mass=1.0, restitution=0.7)
-    cubeBodies.append(cube1)
-
-    # Position and orient third cube
-    startPosCube = cube_locations[2]
-    cube2 = p.createMultiBody(baseCollisionShapeIndex=cubeShape, basePosition=startPosCube,
-                              baseOrientation=startOrientationCube)
-    p.changeDynamics(cube2, -1, mass=1.0, restitution=0.4)
-    cubeBodies.append(cube2)
-
-    # Position and orient fourth cube
-    startPosCube = cube_locations[3]
-    cube3 = p.createMultiBody(baseCollisionShapeIndex=cubeShape, basePosition=startPosCube,
-                              baseOrientation=startOrientationCube)
-    p.changeDynamics(cube3, -1, mass=1.0, restitution=0.1)
-    cubeBodies.append(cube3)
-
-    '''
-    Create and position soft body cubes
-    cube1 = p.loadSoftBody("soft_cube__sf.obj", simFileName="soft_cube.vtk", basePosition=cube_locations[0],
-                            mass=4, useNeoHookean=1, NeoHookeanMu=60, NeoHookeanLambda=600, NeoHookeanDamping=0.001,
-                            collisionMargin=0.006, useSelfCollision=1, frictionCoeff=0.5, repulsionStiffness=800)
-    cubeBodies.append(cube1)
-
-    cube2 = p.loadSoftBody("soft_cube__sf.obj", simFileName="soft_cube.vtk", basePosition=cube_locations[1],
-                            mass=4, useNeoHookean=1, NeoHookeanMu=280, NeoHookeanLambda=600, NeoHookeanDamping=0.001,
-                            collisionMargin=0.006, useSelfCollision=1, frictionCoeff=0.5, repulsionStiffness=700)
-    cubeBodies.append(cube2)
-    '''
-
-    '''
-    # Create and position platform
-    platformBody = p.createCollisionShape(p.GEOM_BOX, halfExtents=[.1, .1, .1])
-    startPosPlatform = platform_location
-    startOrientationPlatform = p.getQuaternionFromEuler([platform_orientation[0], platform_orientation[1],
-                                                         platform_orientation[2]])
-    platform = p.createMultiBody(baseCollisionShapeIndex=platformBody, basePosition=startPosPlatform,
-                                 baseOrientation=startOrientationPlatform)
-    p.changeDynamics(platform, -1, mass=0.0, restitution=0.9)
-    '''
-
-    return cubeBodies, cube_locations
-
-def test_elasticity():
-
-
-    return
 
 
 def run_sample(cubeBodies, cube_positions):
--- Tests/tests.py
+++ Tests/tests.py
@@ -2,19 +2,14 @@ import pybullet as p
 import pybullet_data
 import simulation
 
-# TODO: Drop sphere from 1m, vary elasticity from 0.0-1.0 with 0.1 increments, save to .csv 
-# TODO: Set elasticity of floor to 0.3
-# TODO: Rotate a cube to ~10 angles, then do the same test
-
 # TODO: Find a way to run multiple simulations, save, display as 'video' later
 def main():
     p.connect(p.GUI)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
     p.resetDebugVisualizerCamera(4, 0, -1, [0, 0, 1])
 
-    sphereBodies, sphere_locations = simulation.init_scene()
-    simulation.run_sample(sphereBodies, sphere_locations)
+    spheres, locations = simulation.init_scene()
+    simulation.test_elasticity(spheres[0])
 
     p.disconnect()
     return
