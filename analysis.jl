b292c8a2efd14c2423ef34b75899c279ad9b6e09
jackhdewey <jackhdewey@gmail.com>
2024-03-13 16:00:39 -0400

    Updates to plotting

--- analysis.jl
+++ analysis.jl
@@ -10,32 +10,85 @@ include("Utilities/fileio.jl")
 
 function main()
 
+    plt1 = plot3d( 
+        1,
+        xlim = (-.5, .5),
+        ylim = (-.5, .5),
+        zlim = (0, 1),
+        title = "Particle Filter Flow",
+        legend = false,
+        marker = 2,
+        seriestype=:scatter
+    )
+    plt2 = plot3d( 
+        1,
+        xlim = (-.5, .5),
+        ylim = (-.5, .5),
+        zlim = (0, 1),
+        title = "Particle Filter Flow",
+        legend = false,
+        marker = 2,
+        seriestype=:scatter
+    )
+
     all_files = filter_unwanted_filenames(readdir("BulletData/Intermediate/")) 
+
     sort!(all_files, lt=trial_particle_order)
-    file = all_files[3]
+    file = all_files[30]
 
     #for file in all_files
 
         data = CSV.read(string("BulletData/Intermediate/", file), DataFrame)
 
+        ground_truth = CSV.read("RealFlowData/Cube_Ela3_Var26_observed.csv", DataFrame)
+
+        true_x = []
+        true_y = []
+        true_z = []
+        for i=1:30
+            true_x = [true_x; ground_truth[i, 1]]
+            true_z = [true_z; ground_truth[i, 2]]
+            true_y = [true_y; ground_truth[i, 3]]
+        end
+
+        for i=1:20
+            display(data[data.particle .== i, 5:7])
+            elasticity = data[data.particle .== i, 2]
+            weight = data[data.particle .== i, 3]
+            print(string("Elasticity: ", elasticity[1], "\n"))
+            print(string("Log Weight: ", weight[1], "\n"))
+        end
+
         tokens = split(file, "_")
         time_step = parse(Int64, replace(tokens[4], ".csv" => ""))
         
-        trajectories = Array{Array{Float64, 1}, 1}
-        for i=1:time_step:20*time_step
-            trajectory = []
+        # Iterate over each particle
+        for i=1:time_step:20*time_step            
+
+            x_trajectory = []
+            y_trajectory = []
+            z_trajectory = []
+
+            # Iterate over each time step
             for j=1:time_step
-                col = (i-1) + j
-                x_pos = data[col, 5]
-                y_pos = data[col, 6]
-                z_pos = data[col, 7]
-                trajectory = [trajectory; [x_pos, y_pos, z_pos]]
+                row = (1) + j
+                x_trajectory = [x_trajectory; data[row, 5]]
+                y_trajectory = [y_trajectory; data[row, 6]]
+                z_trajectory = [z_trajectory; data[row, 7]]
             end
-            println(trajectory)
+
+            # Plot the 
+            for k=1:length(x_trajectory)
+                push!(plt1, x_trajectory[k], y_trajectory[k], z_trajectory[k])
+            end
+
+            for k=1:length(true_x)
+                push!(plt2, true_x[k], true_y[k], true_z[k])
+            end
         end
-        
+        display(plt1)
+        display(plt2)
     #end
-    #plt = plot3d()
 end
 
 main()
