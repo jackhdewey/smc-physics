c499b30ddb9bf75e72fa2a30574262dfe382f264
jackhdewey <jackhdewey@gmail.com>
2024-03-15 10:09:57 -0400

    Updated plots

--- Utilities/plots.jl
+++ Utilities/plots.jl
@@ -3,53 +3,6 @@
 using Plots
 using Gen
 
-# Generates an animated 2D plot of height over time 
-@userplot SimPlot
-@recipe function f(cp::SimPlot)
-    z, t = cp.args
-    cs = size(z, 1)
-    k = 10
-    inds = (max(1, t-k):t)
-    n = length(inds)
-    linewidth -->range(0,10, length=n)
-    seriesalpha --> range(0,1,length=n)
-    xguide --> "time"
-    xlims --> (1, 40)
-    yguide --> "height of cube (z)"
-    ylims --> (0, 1.0)
-    label --> false
-    inds, z[inds, :]
-end
-
-# Extract height measurements from the given trace
-function get_zs(trace::Gen.Trace)
-    t, _... = get_args(trace)
-    states = get_retval(trace)
-    zs = Vector{Float64}(undef, t)
-    for i = 1:t
-        zs[i] = states[i].kinematics[1].position[3]
-    end
-    return zs
-end
-
-# Animate a single trajectory
-function animate_trace(trace::Gen.Trace; label="trace")
-    t = first(get_args(trace))
-    zs = reshape(get_zs(trace), (t, 1))
-    @animate for i=2:t
-        simplot(zs, i, label=label)
-    end
-end
-
-# Animate multiple trajectories in one plot
-function animate_traces(traces::Vector{<:Gen.Trace})
-    zzs = reduce(hcat, map(get_zs, traces))
-    t = size(zzs, 1)
-    @animate for i=2:t
-        simplot(zzs, i)
-    end
-end
-
 # Plots
 
 # Plot a single execution trace (particle) of the generative model
@@ -98,4 +51,51 @@ end
 function plot_traj(all_particles)
     traj = filter(:particle => x -> x == 1, all_particles)
     plt = plot3d(traj.x, traj.y, traj.z)
+end
+
+# Generates an animated 2D plot of height over time 
+@userplot SimPlot
+@recipe function f(cp::SimPlot)
+    z, t = cp.args
+    cs = size(z, 1)
+    k = 10
+    inds = (max(1, t-k):t)
+    n = length(inds)
+    linewidth -->range(0,10, length=n)
+    seriesalpha --> range(0,1,length=n)
+    xguide --> "time"
+    xlims --> (1, 40)
+    yguide --> "height of cube (z)"
+    ylims --> (0, 1.0)
+    label --> false
+    inds, z[inds, :]
+end
+
+# Extract height measurements from the given trace
+function get_zs(trace::Gen.Trace)
+    t, _... = get_args(trace)
+    states = get_retval(trace)
+    zs = Vector{Float64}(undef, t)
+    for i = 1:t
+        zs[i] = states[i].kinematics[1].position[3]
+    end
+    return zs
+end
+
+# Animate a single trajectory
+function animate_trace(trace::Gen.Trace; label="trace")
+    t = first(get_args(trace))
+    zs = reshape(get_zs(trace), (t, 1))
+    @animate for i=2:t
+        simplot(zs, i, label=label)
+    end
+end
+
+# Animate multiple trajectories in one plot
+function animate_traces(traces::Vector{<:Gen.Trace})
+    zzs = reduce(hcat, map(get_zs, traces))
+    t = size(zzs, 1)
+    @animate for i=2:t
+        simplot(zzs, i)
+    end
 end
--- analysis.jl
+++ analysis.jl
@@ -10,6 +10,7 @@ include("Utilities/fileio.jl")
 
 function main()
 
+    # Generate plot base
     plt1 = plot3d( 
         1,
         xlim = (-.5, .5),
@@ -20,75 +21,59 @@ function main()
         marker = 2,
         seriestype=:scatter
     )
-    plt2 = plot3d( 
-        1,
-        xlim = (-.5, .5),
-        ylim = (-.5, .5),
-        zlim = (0, 1),
-        title = "Particle Filter Flow",
-        legend = false,
-        marker = 2,
-        seriestype=:scatter
-    )
 
+    # Plot ground truth trajectory
+    ground_truth = CSV.read("RealFlowData/Cube_Ela3_Var26_observed.csv", DataFrame)
+    true_x = []
+    true_y = []
+    true_z = []
+    for i=1:30
+        true_x = [true_x; ground_truth[i, 1]]
+        true_z = [true_z; ground_truth[i, 2]]
+        true_y = [true_y; ground_truth[i, 3]]
+    end
+
+    plot!(plt1, true_x, true_y, true_z, linewidth=3, linecolor=:red)
+
+    # Read all particle files
     all_files = filter_unwanted_filenames(readdir("BulletData/Intermediate/")) 
-
     sort!(all_files, lt=trial_particle_order)
-    file = all_files[30]
 
-    #for file in all_files
+    # For each time step in the first trial
+    for x=1:30
 
+        file = all_files[x]
         data = CSV.read(string("BulletData/Intermediate/", file), DataFrame)
-
-        ground_truth = CSV.read("RealFlowData/Cube_Ela3_Var26_observed.csv", DataFrame)
-
-        true_x = []
-        true_y = []
-        true_z = []
-        for i=1:30
-            true_x = [true_x; ground_truth[i, 1]]
-            true_z = [true_z; ground_truth[i, 2]]
-            true_y = [true_y; ground_truth[i, 3]]
-        end
-
-        for i=1:20
-            display(data[data.particle .== i, 5:7])
-            elasticity = data[data.particle .== i, 2]
-            weight = data[data.particle .== i, 3]
-            print(string("Elasticity: ", elasticity[1], "\n"))
-            print(string("Log Weight: ", weight[1], "\n"))
-        end
-
         tokens = split(file, "_")
         time_step = parse(Int64, replace(tokens[4], ".csv" => ""))
-        
-        # Iterate over each particle
-        for i=1:time_step:20*time_step            
+       
+        # For each particle at this time step
+        for i=1:20    
 
+            particle = data[data.particle .== i, :]
+
+            elasticity = data[data.particle .== i, 2]
+            weight = data[data.particle .== i, 3]
+
+            # @df plot!(plt1, particle[:, 5:7])
+
+            # Iterate over each time step and push to plot
             x_trajectory = []
             y_trajectory = []
             z_trajectory = []
-
-            # Iterate over each time step
-            for j=1:time_step
-                row = (1) + j
-                x_trajectory = [x_trajectory; data[row, 5]]
-                y_trajectory = [y_trajectory; data[row, 6]]
-                z_trajectory = [z_trajectory; data[row, 7]]
+            for row=1:time_step
+                x_trajectory = [x_trajectory; particle[row, 5]]
+                y_trajectory = [y_trajectory; particle[row, 6]]
+                z_trajectory = [z_trajectory; particle[row, 7]]
             end
 
-            # Plot the 
-            for k=1:length(x_trajectory)
-                push!(plt1, x_trajectory[k], y_trajectory[k], z_trajectory[k])
-            end
+            plot!(plt1, x_trajectory, y_trajectory, z_trajectory)
 
-            for k=1:length(true_x)
-                push!(plt2, true_x[k], true_y[k], true_z[k])
-            end
         end
+
+        # Plot the current particles
         display(plt1)
-        display(plt2)
-    #end
+    end
 end
 
 main()
