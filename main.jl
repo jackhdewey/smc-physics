70acfeb84cd23aead463b29e4991759e31f8078c
jackhdewey <jackhdewey@gmail.com>
2024-06-05 15:23:58 -0400

    Model iteration

--- main.jl
+++ main.jl
@@ -18,7 +18,7 @@ include("Utilities/plots.jl")
 function main()
 
     # Select the target object type(s)
-    model_id = "Modelv2"
+    model_id = "Modelv3"
     expt_id = "Exp1"
     target_id = "Sphere"
     output_id = string(model_id, "/", expt_id, "/", target_id)
