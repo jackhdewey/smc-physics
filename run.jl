f2784da04ad1148f59b513e3070a0f649d497c75
jackhdewey <jackhdewey@gmail.com>
2024-07-15 13:58:47 -0400

    Adding parallelism

---
+++ run.jl
@@ -0,0 +1,19 @@
+using Distributed
+addprocs(4)
+
+include("Utilities/fileio.jl")
+
+@everywhere include("main.jl")
+
+expt_id = "Exp1"
+dir = string("RealFlowData/", expt_id, "/")
+
+fnames = readdir(dir)
+fnames = filter_unwanted_filenames(fnames)
+
+sort!(fnames, lt=trial_order)
+
+fnames = [joinpath(expt_id, f) for f in fnames]
+
+# main(fnames[1])
+pmap(main, fnames)
