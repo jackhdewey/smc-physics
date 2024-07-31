4e4b9cfd675a944e75d51f20601ad4f25d860089
Max Siegel <maxs@mit.edu>
2024-07-31 13:04:06 -0400

    particle analysis debug

--- Analysis/particle_analysis.jl
+++ Analysis/particle_analysis.jl
@@ -11,6 +11,8 @@ include("../Utilities/fileio.jl")
 include("../Utilities/plots.jl")
 
 using Plots
+# using PyPlot
+pyplot()
 
 function main()
 
@@ -38,7 +40,7 @@ function main()
 
     # Pull intermediate particle filter state files from directory
     dir = string("Data/BulletData/", output_id, "/Intermediate/")
-    particle_files = filter_unwanted_filenames(readdir(dir)) 
+    particle_files = filter_unwanted_filenames(readdir(dir))
     sort!(particle_files, lt=trial_particle_order)
 
     data = CSV.read(string(dir, particle_files[1]), DataFrame)
@@ -46,10 +48,22 @@ function main()
 
     # For each trajectory
     total_particle_index = 0
+<<<<<<< variant A
+>>>>>>> variant B
+
+    #for i in eachindex(gt_files[1:2])
+======= end
     for i in eachindex(gt_files)
+        plotp = false
+
+
+        if occursin("Ela9_Var1_", gt_files[i])
+            plotp = true
+        end
         tokens = split(gt_files[i], "_")
 
         # Generate plot base
+        if plotp
         plt = plot3d(
             1,
             xlim=(-0.5, 0.5),
@@ -62,23 +76,33 @@ function main()
             size=(1200, 800),
             gridlinewidth=8
         )
+        end
 
         gt_file = string("Data/RealFlowData/", expt_id, "/", gt_files[i])
         ground_truth = CSV.read(gt_file, DataFrame)
 
         # Generate the plot procedurally
         num_timesteps = size(ground_truth)[1]
+        # true_x = zeros(num_timesteps)
+        # true_y = zeros(num_timesteps)
+        # true_z = zeros(num_timesteps)
         true_x = []
         true_y = []
         true_z = []
-        for t = 1:num_timesteps
+        # for t = 1:num_timesteps
+        for t = 1:15
             # for t = 10:11
 
             # Extend ground truth trajectory by one time step and add to plot
             true_x = [true_x; ground_truth[t, 1]]
             true_y = [true_y; ground_truth[t, 2]]
             true_z = [true_z; ground_truth[t, 3]]
+            # true_x[t] = ground_truth[t, 1]
+            # true_y[t] = ground_truth[t, 2]
+            # true_z[t] = ground_truth[t, 3]
+            if plotp
             plot!(plt, true_x, true_y, true_z, linewidth=3, linecolor=:red)
+            end
 
             # Index into correct particle filter file
             particle_index = total_particle_index + t
@@ -100,27 +124,40 @@ function main()
                 x_trajectory = []
                 y_trajectory = []
                 z_trajectory = []
+                # x_trajectory[row] = particle[row, 5]
+                # y_trajectory[row] = particle[row, 6]
+                # z_trajectory[row] = particle[row, 7]
                 for row = 1:t
                     x_trajectory = [x_trajectory; particle[row, 5]]
                     y_trajectory = [y_trajectory; particle[row, 6]]
                     z_trajectory = [z_trajectory; particle[row, 7]]
                 end
+                if plotp
                 plot!(plt, x_trajectory, y_trajectory, z_trajectory)
+                #PyPlot.text3D(0, 0, 0, "test")
+                #annotate!(x_trajectory[end], y_trajectory[end], z_trajectory[end], text(t, 10))
+                # PyPlot.text3D(x_trajectory[end], y_trajectory[end], z_trajectory[end], "1")
+                end
 
             end
 
             # Save and display the plot every fifth time step
             tokens = split(file, "_")
+            if plotp
             if t % plot_interval == 0
                 display(plt)
                 if !interactive
                     savefig(string("Analysis/Plots/", output_id, "/", tokens[2], "_", tokens[3], "_", t))
                 end
             end
-
+        end
         end
         total_particle_index += num_timesteps
     end
 end
 
+# function run_with_profiler()
+# # @profview main()
+# @profile main()
+# end
 main()
