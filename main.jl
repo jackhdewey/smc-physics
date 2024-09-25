5e9c5d3c73865cb896deaf058a40026ef0164b72
jackhdewey <jackhdewey@gmail.com>
2024-09-25 13:37:46 -0400

    Refactoring args into main

--- main.jl
+++ main.jl
@@ -21,6 +21,30 @@ include("Inference/particle_filter.jl")
 include("Utilities/plots.jl")
 include("Utilities/args.jl")
 
+
+@with_kw struct Args
+
+    # Data source
+    gt_source::String = "Bullet"
+
+    # Model parameters
+    model_id::String = "Modelv5"
+    target_id::String = "Cube"
+    noise_id::String = "PosVar05"
+    expt_id::String = "BulletxBullet"
+    output_id::String = string(model_id, "/", target_id, "/", noise_id, "/", expt_id)
+
+    # Inference parameters
+    algorithm::String = "MCMC"    # MCMC, PARTICLE_FILTER, or DEBUG
+    num_particles::Int = 20
+    save_intermediate::Bool = true
+
+    # Prediction parameters
+    predict::Bool = false
+    prediction_timesteps::Int = 90
+
+end
+
 @everywhere begin
 function run(fname, args, w1, w2)
     
